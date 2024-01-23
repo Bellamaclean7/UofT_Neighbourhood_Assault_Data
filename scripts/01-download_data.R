49f50bd8d7e3709ac682ed3c828b82a7395b5bec
Bellamaclean7 <bellamaclean7@gmail.com>
2024-01-23 04:06:24 +0000

    commiting the dataset from open data toronto and saving it to inputs/data/raw_data.csv

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,34 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads, saves, and reads data from the OpenDataToronto portal
+# Author: Bella MacLean
+# Date: 20 January 2024
+# Contact: bella.maclean@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: none
 
+# code taken directly from https://open.toronto.ca/dataset/neighbourhood-crime-rates/
 
-#### Workspace setup ####
+### Workspace setup ####
+install.packages("opendatatoronto")
+install.packages("dplyr")
+install.packages("readr")  
+
 library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# get package
+package <- show_package("neighbourhood-crime-rates")
+package
 
+# get all resources for this package
+resources <- list_package_resources("neighbourhood-crime-rates")
 
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data
 
-         
+# Write the data to a CSV file
+write_csv(data, "inputs/data/raw_data.csv")
