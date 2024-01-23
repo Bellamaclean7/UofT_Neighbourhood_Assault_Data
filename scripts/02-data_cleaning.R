223f2e9fda2d057dc97efbdcde85851b5c5465b2
Bellamaclean7 <bellamaclean7@gmail.com>
2024-01-23 04:14:56 +0000

    commiting first rounf of cleaned data and saving to outputs/data/cleaned_data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,37 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw data to prepare for data anlysis 
+# Author: Bella MacLean
+# Date: 21 January 2024
+# Contact: bella.maclean@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: none
 
 #### Workspace setup ####
 library(tidyverse)
+library(readr)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("inputs/data/raw_data.csv")
+View(raw_data)
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+# Select the specified columns and rename AREA_NAME
+assault_rate_data <- raw_data |>
+  select(AREA_NAME, 
+         ASSAULT_RATE_2014, ASSAULT_RATE_2015, ASSAULT_RATE_2016, 
+         ASSAULT_RATE_2017, ASSAULT_RATE_2018, ASSAULT_RATE_2019, 
+         ASSAULT_RATE_2020, ASSAULT_RATE_2021, ASSAULT_RATE_2022, 
+         ASSAULT_RATE_2023) |>
+  rename(Neighbourhood = AREA_NAME)
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+# Filter rows for specific neighborhoods
+uoft_neighbourhoods <- assault_rate_data |>
+  filter(Neighbourhood %in% c("Kensington-Chinatown", "University", "Bay-Cloverhill", "Annex"))
+
+# View the first few rows of the dataset with renamed variables
+head(uoft_neighbourhoods)
+
+# Save the cleaned assault rate data
+write_csv(
+  x = uoft,
+  file = "outputs/data/cleaned_data.csv"
+)
